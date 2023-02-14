0e58c3d288da37a3fec89c037fb5cf6f0f5deaaa
Djo-75 <j.pecquet@live.fr>
2023-02-14 16:07:02 +0100

    first_commit

---
+++ app/controllers/gossip_page_controller.rb
@@ -0,0 +1,11 @@
+class GossipPageController < ApplicationController
+  
+  def gossip
+    @id = (params[:id]).to_i
+    @all_gossips = []
+    (Gossip.all).each do |instance|
+      @all_gossips << instance
+    end
+  end
+
+end
--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,4 +1,8 @@
 class Gossip < ApplicationRecord
+    validates :title, presence: true # rend la présence du titre obligatoire
+    validates :title, length: { in: 6..20 }
+    validates :content, presence: true
+
     belongs_to :user, required: false
     belongs_to :tag_gossip, required: false
     has_many :tags, through: :tag_gossips
