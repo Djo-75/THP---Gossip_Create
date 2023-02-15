26992669d9a085f0e9e2d8bb456d3542a3bc1bae
Djo-75 <j.pecquet@live.fr>
2023-02-15 20:19:19 +0100

    up to 2.4

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -26,7 +26,7 @@ class GossipsController < ApplicationController
     end
 
     def edit
-      @gossip = Gossip.find(params[:id])
+      @gossip = Gossip.find(params[:id]) #
     end
     
     def update
