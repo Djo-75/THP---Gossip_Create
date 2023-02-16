80e15b4fbf14046f3c12ec4c89b53df244580269
Djo-75 <j.pecquet@live.fr>
2023-02-16 16:24:09 +0100

    up to 2.2.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,5 @@
 class SessionsController < ApplicationController
-
-    def index
-    end
-
-    def show
-    end
+  before_action :authenticate_user, only: [:index]
 
     def new
     end
@@ -12,27 +7,32 @@ class SessionsController < ApplicationController
     def create
       # cherche s'il existe un utilisateur en base avec l’e-mail
 
-    #   user = User.find_by(email: email_dans_ton_params)
+      user = User.find_by(email: params[:email])
+      # puts "||||||||||||||||||||||||||||||||||||"
+      # puts user.id
+      # puts user.first_name
+      # puts user.last_name
+      # puts user.password_digest
+      # puts "||||||||||||||||||||||||||||||||||||"
 
       # on vérifie si l'utilisateur existe bien ET si on arrive à l'authentifier (méthode bcrypt) avec le mot de passe 
 
-    #   if user && user.authenticate(password_dans_ton_params)
-    #     session[:user_id] = user.id
+      if user.email == params[:email] && user.authenticate(params[:password])
+        session[:user_id] = user.id
+      # puts "*****************"
+      # puts "ça marche"
+      # puts "!!!!!!!!!!!!!!!!"
 
+      redirect_to root_path
         # redirige où tu veux, avec un flash ou pas
 
-    #   else
-    #     flash.now[:danger] = 'Invalid email/password combination'
-    #     render 'new'
-    #   end
-    end
-
-    def edit
-    end
-
-    def update
+      else
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+      end
     end
 
     def destroy
+      session.delete(:user_id)
     end
 end
